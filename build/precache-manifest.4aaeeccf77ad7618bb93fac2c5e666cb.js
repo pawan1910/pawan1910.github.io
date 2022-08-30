fdf5165a054fe91d5a89ff1fb3793df709e114ac
pawan1910 <pawansingh1910@gmail.com>
2022-08-30 09:49:31 +0530

    build

---
+++ build/precache-manifest.4aaeeccf77ad7618bb93fac2c5e666cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf55cfd124865706b798",
+    "url": "/React_assg/build/static/css/main.626fdb81.chunk.css"
+  },
+  {
+    "revision": "bf55cfd124865706b798",
+    "url": "/React_assg/build/static/js/main.bf55cfd1.chunk.js"
+  },
+  {
+    "revision": "f3ba07ae8adf9c0824d2",
+    "url": "/React_assg/build/static/js/1.f3ba07ae.chunk.js"
+  },
+  {
+    "revision": "f6807161a218d15503df",
+    "url": "/React_assg/build/static/js/runtime~main.f6807161.js"
+  },
+  {
+    "revision": "414946edac742595284970647b3355a5",
+    "url": "/React_assg/build/index.html"
+  }
+];
